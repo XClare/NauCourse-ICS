703181847be25c68836607f508749ff6d7a03a5c
XFY9326 <1069665464@qq.com>
2020-02-07 15:56:02 +0800

    Support args launch
    Signed-off-by: XFY9326 <1069665464@qq.com>

--- src/main/kotlin/tool/naucourse/ics/Main.kt
+++ src/main/kotlin/tool/naucourse/ics/Main.kt
@@ -22,28 +22,58 @@ object Main {
 
     @JvmStatic
     fun main(args: Array<String>) {
-        runProcess()
+        if (args.size >= 3) {
+            val userId = args[0].trim()
+            val userPw = args[1].trim()
+            val type = when (args[2].trim()) {
+                "1" -> NetDataType.THIS_TERM_COURSE
+                "2" -> NetDataType.NEXT_TERM_COURSE
+                "3" -> NetDataType.EXAM
+                else -> throw IllegalArgumentException("未知数据类型参数：${args[2]}")
+            }
+            if (type == NetDataType.NEXT_TERM_COURSE && args.size >= 5) {
+                var startDay: Date? = null
+                var endDay: Date? = null
+                try {
+                    startDay = Constants.Time.DATE_FORMAT_YMD.parse(args[3])!!
+                    endDay = Constants.Time.DATE_FORMAT_YMD.parse(args[4])!!
+                } catch (e: Exception) {
+                    println("输入日期的格式有误！")
+                }
+                runProcess(userId, userPw, type, startDay, endDay)
+            } else {
+                runProcess(userId, userPw, type)
+            }
+        } else {
+            runProcess()
+        }
         finishRunning()
     }
 
-    private fun runProcess() {
+    private fun runProcess(
+        userId: String? = null,
+        userPw: String? = null,
+        type: NetDataType? = null,
+        startDay: Date? = null,
+        endDay: Date? = null
+    ) {
         showBeforeLoginText()
-        if (askForLogin()) {
+        if (askForLogin(userId, userPw)) {
             val courseDataResult: ContentResult<CourseSet>
             val termDateResult: ContentResult<TermDate>
             val examArrangeList: ContentResult<Array<Exam>>
             try {
                 val studentInfo = StudentIndex.getContentData()
-                when (askCourseTableType()) {
-                    CourseTableType.THIS_TERM -> {
+                when (askCourseTableType(type)) {
+                    NetDataType.THIS_TERM_COURSE -> {
                         println("\n正在从教务获取学期数据... ...")
                         termDateResult = TermInfo.getContentData()
                         println("正在从教务获取本学期课程数据... ...")
                         courseDataResult = MyCourseScheduleTable.getContentData()
                         convertCourse(studentInfo, courseDataResult, termDateResult)
                     }
-                    CourseTableType.NEXT_TERM -> {
-                        termDateResult = askForTermDate()
+                    NetDataType.NEXT_TERM_COURSE -> {
+                        termDateResult = askForTermDate(startDay, endDay)
                         println("\n正在从教务获取下学期课程数据... ...")
                         courseDataResult = MyCourseScheduleTableNext.getContentData()
                         if (!courseDataResult.isSuccess
@@ -55,7 +85,7 @@ object Main {
                             convertCourse(studentInfo, courseDataResult, termDateResult)
                         }
                     }
-                    CourseTableType.EXAM -> {
+                    NetDataType.EXAM -> {
                         println("\n正在从教务获取学期数据... ...")
                         termDateResult = TermInfo.getContentData()
                         println("正在从教务获取考试日程数据... ...")
@@ -80,6 +110,11 @@ object Main {
         courseDataResult: ContentResult<CourseSet>,
         termDateResult: ContentResult<TermDate>
     ) {
+        if (studentInfo.isSuccess) {
+            println("当前用户：${studentInfo.contentData!!.name.second}")
+        } else {
+            println("未知用户！")
+        }
         if (courseDataResult.isSuccess && termDateResult.isSuccess) {
             val courseData = courseDataResult.contentData!!
             val termDate = termDateResult.contentData!!
@@ -141,21 +176,30 @@ object Main {
         println("版本：${Constants.VERSION}")
         println("本应用基于GPL3.0开源协议开源")
         println("开源地址：https://github.com/XClare/NauCourse-ICS")
-        println("\n-> 使用前须知：由于学校教考日程变动，因此可能会导致时间不准确，如出现问题，后果自负 <-\n")
+        println("\n-> 使用前须知：由于学校教考日程变动，因此可能会导致时间不准确，如出现问题，后果自负 <-")
+        println("-> 使用时请全程保持网络可用状态！ <-\n")
     }
 
-    private fun askForLogin(): Boolean {
-        println("请输入您的用户登录信息，并以回车表示结束")
-        print("学号：")
-        val userId = readLine()!!
-        print("SSO登录密码：")
-        val userPw = readLine()!!
+    private fun askForLogin(userId: String? = null, userPw: String? = null): Boolean {
+        val loginId: String
+        val loginPw: String
+        if (userId != null && userPw != null) {
+            loginId = userId
+            loginPw = userPw
+        } else {
+            println("请输入您的用户登录信息，并以回车表示结束")
+            print("学号：")
+            loginId = readLine()!!
+            print("SSO登录密码：")
+            loginPw = readLine()!!
+
+        }
 
         println("正在登录中... ...")
         NauNetworkManager.setLoginInfo(
             LoginInfo(
-                userId,
-                userPw
+                loginId,
+                loginPw
             )
         )
         val loginResponse = NauNetworkManager.getJwcClient().login()
@@ -168,71 +212,93 @@ object Main {
         }
     }
 
-    private fun askForTermDate(): ContentResult<TermDate> {
-        println()
-        println("请输入学期开始时间与学期结束时间，并以回车表示结束")
-        println("输入格式示例：2020-01-01")
+    private fun askForTermDate(startDay: Date? = null, endDay: Date? = null): ContentResult<TermDate> {
         val calendar = Calendar.getInstance(Locale.CHINA)
-
-        var startDate: Date? = null
-        while (startDate == null) {
-            print("学期开始日期(必须是周一)：")
-            val dateStr = readLine()!!
-            try {
-                startDate = Constants.Time.DATE_FORMAT_YMD.parse(dateStr)!!
-                calendar.time = startDate
-                if (calendar.get(Calendar.DAY_OF_WEEK) != Calendar.MONDAY) {
-                    startDate = null
-                    println("学期开始日期不是周一，请重新输入")
-                }
-            } catch (e: Exception) {
-                println("日期格式错误，请重新输入")
+        if (startDay != null && endDay != null) {
+            calendar.time = startDay
+            if (calendar.get(Calendar.DAY_OF_WEEK) != Calendar.MONDAY) {
+                println("学期开始日期不是周一!")
+                return ContentResult(false, contentErrorResult = ContentErrorReason.OPERATION)
             }
-        }
 
-        var endDate: Date? = null
-        while (endDate == null) {
-            print("学期结束日期(必须是周日)：")
-            val dateStr = readLine()!!
-            try {
-                endDate = Constants.Time.DATE_FORMAT_YMD.parse(dateStr)!!
-                calendar.time = endDate
-                if (calendar.get(Calendar.DAY_OF_WEEK) != Calendar.SUNDAY) {
-                    endDate = null
-                    println("学期结束日期不是周日，请重新输入")
-                } else if (startDate.time >= endDate.time) {
-                    endDate = null
-                    println("学期开始日期大于结束日期，请重新输入")
-                }
-            } catch (e: Exception) {
-                println("日期格式错误，请重新输入")
+            calendar.time = endDay
+            if (calendar.get(Calendar.DAY_OF_WEEK) != Calendar.SUNDAY) {
+                println("学期结束日期不是周日!")
+                return ContentResult(false, contentErrorResult = ContentErrorReason.OPERATION)
             }
-        }
 
-        return ContentResult(
-            true,
-            contentData = TermDate(0, startDate, endDate)
-        )
+            return ContentResult(
+                true,
+                contentData = TermDate(0, startDay, endDay)
+            )
+        } else {
+            println()
+            println("请输入学期开始时间与学期结束时间，并以回车表示结束")
+            println("输入格式示例：2020-01-01")
+
+            var startDate: Date? = null
+            while (startDate == null) {
+                print("学期开始日期(必须是周一)：")
+                val dateStr = readLine()!!
+                try {
+                    startDate = Constants.Time.DATE_FORMAT_YMD.parse(dateStr)!!
+                    calendar.time = startDate
+                    if (calendar.get(Calendar.DAY_OF_WEEK) != Calendar.MONDAY) {
+                        startDate = null
+                        println("学期开始日期不是周一，请重新输入")
+                    }
+                } catch (e: Exception) {
+                    println("日期格式错误，请重新输入")
+                }
+            }
+
+            var endDate: Date? = null
+            while (endDate == null) {
+                print("学期结束日期(必须是周日)：")
+                val dateStr = readLine()!!
+                try {
+                    endDate = Constants.Time.DATE_FORMAT_YMD.parse(dateStr)!!
+                    calendar.time = endDate
+                    if (calendar.get(Calendar.DAY_OF_WEEK) != Calendar.SUNDAY) {
+                        endDate = null
+                        println("学期结束日期不是周日，请重新输入")
+                    } else if (startDate.time >= endDate.time) {
+                        endDate = null
+                        println("学期开始日期大于结束日期，请重新输入")
+                    }
+                } catch (e: Exception) {
+                    println("日期格式错误，请重新输入")
+                }
+            }
+            return ContentResult(
+                true,
+                contentData = TermDate(0, startDate, endDate)
+            )
+        }
     }
 
-    private fun askCourseTableType(): CourseTableType {
-        println()
-        println("请选择您想要获取的课程类别：")
-        println("1. 本学期课程")
-        println("2. 下学期课程")
-        println("3. 本学期考试日程")
-        println("请输入类别编号（如 1）后以回车表示确定")
-        var output: CourseTableType? = null
-        while (output == null) {
-            print("请选择：")
-            when (readLine()!!.toInt()) {
-                1 -> output = CourseTableType.THIS_TERM
-                2 -> output = CourseTableType.NEXT_TERM
-                3 -> output = CourseTableType.EXAM
-                else -> println("你输入了错误的参数！")
+    private fun askCourseTableType(type: NetDataType? = null): NetDataType {
+        if (type == null) {
+            println()
+            println("请选择您想要获取的课程类别：")
+            println("1. 本学期课程")
+            println("2. 下学期课程")
+            println("3. 本学期考试日程")
+            println("请输入类别编号（如 1）后以回车表示确定")
+            var output: NetDataType? = null
+            while (output == null) {
+                print("请选择：")
+                when (readLine()!!.toInt()) {
+                    1 -> output = NetDataType.THIS_TERM_COURSE
+                    2 -> output = NetDataType.NEXT_TERM_COURSE
+                    3 -> output = NetDataType.EXAM
+                    else -> println("你输入了错误的参数！")
+                }
             }
+            return output
+        } else {
+            return type
         }
-        return output
     }
 
     private fun finishRunning() {
@@ -258,9 +324,9 @@ object Main {
         return file.parentFile
     }
 
-    private enum class CourseTableType {
-        THIS_TERM,
-        NEXT_TERM,
+    private enum class NetDataType {
+        THIS_TERM_COURSE,
+        NEXT_TERM_COURSE,
         EXAM
     }
 }
